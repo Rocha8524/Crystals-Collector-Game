dbc8cfda343b3fc201000d17c51ab564cf27aa9d
Alex Rocha <rocha8524@gmail.com>
2020-05-05 16:26:05 -0400

    Format document

--- assets/jquery/game.js
+++ assets/jquery/game.js
@@ -1,104 +1,106 @@
 // Make function available after the document is loaded.
 jQuery.noConflict()(function ($) {
-$(document).ready(function() {
+    $(document).ready(function () {
 
-// Set up varaiables for the game score and wins/losses tallies
-var userWins = 0;
-// $("#user-wins").text("Wins: " + userWins);
+        // Set up varaiables for the game score and wins/losses tallies
+        var userWins = 0;
 
-var userLosses = 0;
-// $("#user-losses").text("Losses: " + userLosses);
+        var userLosses = 0;
 
-var userScore = 0;
-    $("#your-score").text("Your Score: " + userScore)
+        var userScore = 0;
+        $("#your-score").text("Your Score: " + userScore)
 
-var totalScore = Math.floor(Math.random() * 111) + 19;
-    $("#target-score").text("Target Score: " + totalScore);
+        var totalScore = Math.floor(Math.random() * 111) + 19;
+        $("#target-score").text("Target Score: " + totalScore);
 
-// Set up crystal variables
-var greenGem = Math.floor(Math.random() * 12) + 1;
-var yellowGem = Math.floor(Math.random() * 12) +1;
-var blueGem = Math.floor(Math.random() * 12) + 1;
-var purpleGem = Math.floor(Math.random() * 12) + 1;
+        // Set up crystal variables
+        var greenGem = Math.floor(Math.random() * 12) + 1;
+        var yellowGem = Math.floor(Math.random() * 12) + 1;
+        var blueGem = Math.floor(Math.random() * 12) + 1;
+        var purpleGem = Math.floor(Math.random() * 12) + 1;
 
 
-// Reset game scores and emerald values after each win and loss
-function resetGame () {
-    userScore = 0;
-    $("#your-score").text("Your Score: " + userScore)
-    totalScore = Math.floor(Math.random() * 111) + 19;
-    $("#target-score").text("Target Score: " + totalScore);
-    blueGem = Math.floor(Math.random() * 12) + 1;
-    greenGem = Math.floor(Math.random() * 12) + 1;
-    yellowGem = Math.floor(Math.random() * 12) + 1;
-    purpleGem = Math.floor(Math.random() * 12) + 1;
-}
+        // Reset game scores and emerald values after each win and loss
+        function resetGame() {
 
-// Emerald click functions and values for each game
-$("#blue-emerald").click(function() {
-    userScore = blueGem + userScore;
-    $("#your-score").text("Your Total Score Is: " + userScore);
-    
-    if (userScore == totalScore) {
-        userWins = userWins + 1;
-        $("#user-wins").text("Wins: " + userWins);
-        resetGame();
-    }   
-    if (userScore > totalScore)   {
-        userLosses = userLosses + 1;
-        $("#user-losses").text("Losses: " + userLosses);
-        resetGame();
-    }
-});   
+            userScore = 0;
 
-$("#green-emerald").click(function() {
-    userScore = greenGem + userScore;
-    $("#your-score").text("Your Total Score Is: " + userScore);
-        
-    if (userScore == totalScore) {
-        userWins = userWins + 1;
-        $("#user-wins").text("Wins: " + userWins);
-        resetGame();
+            $("#your-score").text("Your Score: " + userScore)
+            totalScore = Math.floor(Math.random() * 111) + 19;
 
-}   if (userScore > totalScore)   {
-        userLosses = userLosses + 1;
-        $("#user-losses").text("Losses: " + userLosses);
-        resetGame();
-    }
-});  
+            $("#target-score").text("Target Score: " + totalScore);
 
-$("#yellow-emerald").click(function() {
-    userScore = yellowGem + userScore;
-    $("#your-score").text("Your Total Score Is: " + userScore);
-            
-    if (userScore == totalScore) {
-        userWins = userWins + 1;
-        $("#user-wins").text("Wins: " + userWins);
-        resetGame();
-    
-}   if (userScore > totalScore)   {
-        userLosses = userLosses + 1;
-        $("#user-losses").text("Losses: " + userLosses);
-        resetGame();
-    }
-});     
+            blueGem = Math.floor(Math.random() * 12) + 1;
+            greenGem = Math.floor(Math.random() * 12) + 1;
+            yellowGem = Math.floor(Math.random() * 12) + 1;
+            purpleGem = Math.floor(Math.random() * 12) + 1;
+        }
 
-$("#purple-emerald").click(function() {
-    userScore = purpleGem + userScore;
-    $("#your-score").text("Your Total Score Is: " + userScore);
+        // Emerald click functions and values for each game
+        $("#blue-emerald").click(function () {
+            userScore = blueGem + userScore;
+            $("#your-score").text("Your Total Score Is: " + userScore);
 
-    if (userScore == totalScore) {
-        userWins = userWins + 1;
-        $("#user-wins").text("Wins: " + userWins);
-        resetGame();
+            if (userScore == totalScore) {
+                userWins++;
+                $("#user-wins").text("Wins: " + userWins);
+                resetGame();
+            }
+            if (userScore > totalScore) {
+                userLosses++;
+                $("#user-losses").text("Losses: " + userLosses);
+                resetGame();
+            }
+        });
 
-}   if (userScore > totalScore) {
-        userLosses = userLosses + 1;
-        $("#user-losses").text("Losses: " + userLosses);
-        resetGame();
-    }
-});  
+        $("#green-emerald").click(function () {
+            userScore = greenGem + userScore;
+            $("#your-score").text("Your Total Score Is: " + userScore);
 
-});
+            if (userScore == totalScore) {
+                userWins++;
+                $("#user-wins").text("Wins: " + userWins);
+                resetGame();
+
+            } if (userScore > totalScore) {
+                userLosses++;
+                $("#user-losses").text("Losses: " + userLosses);
+                resetGame();
+            }
+        });
+
+        $("#yellow-emerald").click(function () {
+            userScore = yellowGem + userScore;
+            $("#your-score").text("Your Total Score Is: " + userScore);
+
+            if (userScore == totalScore) {
+                userWins++;
+                $("#user-wins").text("Wins: " + userWins);
+                resetGame();
+
+            } if (userScore > totalScore) {
+                userLosses++;
+                $("#user-losses").text("Losses: " + userLosses);
+                resetGame();
+            }
+        });
+
+        $("#purple-emerald").click(function () {
+            userScore = purpleGem + userScore;
+            $("#your-score").text("Your Total Score Is: " + userScore);
+
+            if (userScore == totalScore) {
+                userWins++;
+                $("#user-wins").text("Wins: " + userWins);
+                resetGame();
+
+            } if (userScore > totalScore) {
+                userLosses++;
+                $("#user-losses").text("Losses: " + userLosses);
+                resetGame();
+            }
+        });
+
+    });
 
 });
